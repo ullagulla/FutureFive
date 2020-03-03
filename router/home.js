b3c3736967ad6d1f5a419ad8c8c4d9413613ad5d
Jonathan <Hannerfeldt@hotmail.se>
2020-03-03 13:53:08 +0100

    home

---
+++ router/home.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+
+    res.render('shop/main.ejs')
+})
+
+module.exports = router
