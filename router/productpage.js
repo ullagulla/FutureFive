2b6233ca1f018d0c87142e0d25d2f814f0966f52
ullagulla <ulrikaa91@hotmail.com>
2020-03-06 11:47:38 +0100

    adding specific productpage and styling

--- router/productpage.js
+++ router/productpage.js
@@ -1,102 +1,27 @@
 const express = require('express')
-const app = express()
 const router = express.Router()
 const Product = require("../models/product")
 
-//Jonis kod nedan
-
-// const productsArray = [{
-//         name: "Voodoo doll",
-//         price: 5 ,
-//         imageUrl: "/img/voodoo1.jpg"
-//     },
-//     {
-//         name: "Voodoo dolls 3-pack",
-//         price: 10 ,
-//         imageUrl: "/img/voodoo2.jpg"
-//     },
-//     {
-//         name: "Voodoo doll döskalle",
-//         price: 10 ,
-//         imageUrl: "/img/voodoo3.jpg"
-//     },
-//     {
-//         name: "Drömfångare",
-//         price: 10 ,
-//         imageUrl: "/img/dreamcatcher2.jpg"
-//     },
-//     {
-//         name: "Shrunken head",
-//         price: 10 ,
-//         imageUrl: "/img/shrunkenhead1.jpg"
-//     },
-//     {
-//         name: "Shrunken head",
-//         price: 10 ,
-//         imageUrl: "/img/shrunkenhead2.jpg"
-//     },
-//     {
-//         name: "Love potion",
-//         price: 10 ,
-//         imageUrl: "/img/potion1.jpg"
-//     },
-//     {
-//         name: "Potion kit",
-//         price: 10 ,
-//         imageUrl: "/img/potion3.jpg"
-//     },
-//     {
-//         name: "Ouija board",
-//         price: 10 ,
-//         imageUrl: "/img/ouija-board2.jpg"
-//     },
-//     {
-//         name: "Ouija board",
-//         price: 10 ,
-//         imageUrl: "/img/Ouija-board3.jpg"
-//     },
-//     {
-//         name: "Ouija board",
-//         price: 10 ,
-//         imageUrl: "/img/ouija-board1.jpg"
-//     },
-//     {
-//         name: "Ouija board",
-//         price: 10 ,
-//         imageUrl: "/img/ouija-board4.jpg"
-//     },
-// ]
-
-const products = new Product({
-    productsArray
-})
-
-console.log(productsArray)
-
-// products.save(function (err) {
-//     if (err) return console.log(err);
-
-//     console.log('products saved to the database');
-//     return res.redirect('/products');
-// })
+const items = 8
 
 router.get('/products', async (req, res) => {
-    /*const sorted = req.query.sort;
-    const page = req.query.page;
-    const ourProducts = await Product.find().sort({name:sorted})
+    const page = req.query.page
+    const products = await Product
+    .find()
     .skip(  (page-1) * items)
-    .limit(items)*/
-    
-        // productsArray.forEach( async (e)=>{
-        //     await new Product({
-        //         name:e.name,
-        //         price:e.price,
-        //         imageUrl:e.imageUrl
-        //     }).save()
-        // })
+    .limit(items)
     res.render('shop/products.ejs', {
-        productsArray
+        products
     })
+})
+
+//specific product page below
+
+router.get("/productpage/:id", async (req, res) =>{ //Uppdaterar data
+
+    const product = await Product.findById({_id:req.params.id})
+
+    res.render("shop/productpage.ejs", {product})
 })
 
 module.exports = router
