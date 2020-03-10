538645232fe032f71b863e11f14807b64ef1508a
ullagulla <ulrikaa91@hotmail.com>
2020-03-10 16:41:28 +0100

    changing pagination

--- router/productpage.js
+++ router/productpage.js
@@ -2,26 +2,41 @@ const express = require('express')
 const router = express.Router()
 const Product = require("../models/product")
 
-const items = 8
-
 router.get('/products', async (req, res) => {
-    const page = req.query.page
-    const products = await Product
-    .find()
-    .skip(  (page-1) * items)
-    .limit(items)
-    res.render('shop/products.ejs', {
-        products
+    const currentPage = req.query.page || 1;
+    const productPerPage = 8;
+    const allProducts = await Product.find()
+    const allItems = await Product
+        .find()
+        .skip((currentPage - 1) * productPerPage)
+        .limit(productPerPage)
+    const pageCount = Math.ceil(allProducts.length / productPerPage)
+    res.render("shop/products.ejs", {
+        allItems,
+        pageCount,
+        currentPage
     })
+    // const page = req.query.page
+    // const products = await Product
+    // .find()
+    // .skip(  (page-1) * items)
+    // .limit(items)
+    // res.render('shop/products.ejs', {
+    //     products
+    // })
 })
 
 //specific product page below
 
-router.get("/productpage/:id", async (req, res) =>{ //Uppdaterar data
+router.get("/productpage/:id", async (req, res) => {
 
-    const product = await Product.findById({_id:req.params.id})
+    const product = await Product.findById({
+        _id: req.params.id
+    })
 
-    res.render("shop/productpage.ejs", {product})
+    res.render("shop/productpage.ejs", {
+        product
+    })
 })
 
 module.exports = router
