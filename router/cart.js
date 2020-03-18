a180e3437502a9940327cdb98c2f14bc4139347b
ullagulla <ulrikaa91@hotmail.com>
2020-03-18 15:36:49 +0100

    splicing in cart

--- router/cart.js
+++ router/cart.js
@@ -28,4 +28,23 @@ router.get("/cartAdd/:id", verifyToken, async (req, res) => {
 
 })
 
+// router.get("/delete/:id", verifyToken, async (req, res)=> { //Tar bort data från databasen
+//     const user = await User.findOne({ _id: req.body.user._id });
+
+//     for (let i = 0; i < user.cart.length; i++) {
+
+//         const userCart = user.cart;
+//         console.log(userCart);
+//         userCart.splice(req.params.id[i]);
+//         console.log(userCart);
+//         await user.save();
+//         console.log(user);
+        
+//     }
+
+
+//     res.redirect("/cart")
+
+// })
+
 module.exports = router
