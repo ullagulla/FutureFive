6a81419274023073554eade57fa207d6ab5ac675
ullagulla <ulrikaa91@hotmail.com>
2020-03-24 12:38:05 +0100

    responsive changes on navbar, productpage, cart and checkout

--- cypress/integration/products_ui_spec.js
+++ cypress/integration/products_ui_spec.js
@@ -1,14 +1,3 @@
-describe("Css test for app", () => {
-
-    it("Should have rgb(35, 31, 32) background-color", () => {
-
-        cy.visit("http://localhost:8000/")
-
-        cy.get("body").should("have.css", "background-color", "rgb(35, 31, 32)")
-
-    })
-})
-
 describe("UI test for product page", () => {
 
     it("Should load product page", () => {
